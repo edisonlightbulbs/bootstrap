2c4447de0a2f5a29633299328e19d5d67628c0ff
Everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2020-06-28 10:35:58 +0200

    fixing font.sh

--- offline/Ubuntu/font.sh
+++ offline/Ubuntu/font.sh
@@ -6,16 +6,19 @@
 # and update LINK variable
 
 LINK='https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip'
-LOCAL_FONT_DIR=' ~/.local/share/fonts/'
+LOCAL_FONT_DIR=$HOME/.local/share/fonts/
+
 
 # -- download font
 wget $LINK
 
 
+
 # -- unzip in font directory
-mv FiraCode.zip $LOCAL_FONT_DIR
+sudo mkdir $LOCAL_FONT_DIR
+sudo mv FiraCode.zip $LOCAL_FONT_DIR
 cd $LOCAL_FONT_DIR
-unzip FiraCode.zip
+sudo unzip FiraCode.zip
 
 
 # --rebuild for cache
