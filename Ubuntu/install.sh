d7e2d1409878db79e87b1fd36c6bd2c256415fb2
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2020-09-20 16:51:21 +0200

    update

--- Ubuntu/install.sh
+++ Ubuntu/install.sh
@@ -5,33 +5,25 @@
 #   3. pipPackages
 #   4. npnPackages
 
+#  We use awk to pre-process each line in a given text file, handing over the
+#  output of each line to xargs. AWK Allows us to process each line and hand it
+#  directly to xargs, leaving STDIN unchanged.
+#    [ -a ] read directly from file and leave STDIN unchanged when reading each
+#    line as a  command.
+#    [ -r ] do not run if line empty is empty.
+
 sudo apt update
 
 # -- install apt packages
-xargs -a <(awk '! /^ *(#|$)/' "apt.txt") -r -- sudo apt-get -y install
-
-# -- install snap packages
-xargs -a <(awk '! /^ *(#|$)/' "snap.txt") -r -- sudo snap install
+xargs -a <(awk '! /^ *(#|$)/' "packages/aptPackages.txt") -r -- sudo apt-get -y install
 
 # -- pip install
-xargs -a <(awk '! /^ *(#|$)/' "pip.txt") -r -- sudo pip3 install
+xargs -a <(awk '! /^ *(#|$)/' "packages/npmPackages.txt") -r -- sudo pip3 install
 
 ## -- npm install
-xargs -a <(awk '! /^ *(#|$)/' "npm.txt") -r -- sudo npm install -g
+xargs -a <(awk '! /^ *(#|$)/' "packages/pipPackages.txt") -r -- sudo npm install -g
 
 # -- python3 install
-xargs -a <(awk '! /^ *(#|$)/' "python3.txt") -r sudo python3 -m pip install
+xargs -a <(awk '! /^ *(#|$)/' "packages/py3Packages.txt") -r sudo python3 -m pip install
 
 sudo chsh -s /usr/bin/zsh root
-
-#          xargs builds and executes commands from STDIN
-#   [ -a ] reading directly form the file, i.e., STDIN remains unchanged
-#          when commands are run.
-#   [ -r ] do not run if empty
-
-#           We use awk to pre-process each line in a given text file, handing
-#           over the output of each line to xargs. Piping the file directly to
-#           xargs would change STDIN (undesired!).
-
-#           awk allows us to process each line and hand it directly to xargs,
-#           leaving STDIN unchanged and ready for the <command>
